58081f617e8098bb423ae758b737a3715239f159
fbaliarda <33381234+fbaliarda@users.noreply.github.com>
2022-03-05 14:39:06 -0300

    Analyze statistics and script arguments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
 
 ALCOHOL_COLUMN = 'Alcohol'
 FAT_COLUMN = 'Grasas_sat'
@@ -7,11 +8,20 @@ CALORIES_COLUMN = 'Calorías'
 SEX_COLUMN = 'Sexo'
 
 
-def replace_by_mean(data, column, empty_value=999.99):
+def replace_by_statistic(statistic, data, column, empty_value=999.99):
     filtered_data = data[data[column] != empty_value]
-    mean = filtered_data[column].mean()
-    data.loc[data[column] == empty_value, column] = mean
-    return data
+
+    statistic_value = 0
+    if statistic == 'mean':
+        statistic_value = filtered_data[column].mean()
+    elif statistic == 'mode':
+        statistic_value = filtered_data[column].mode()[0]
+    elif statistic == 'median':
+        statistic_value = filtered_data[column].median()
+
+    data.loc[data[column] == empty_value, column] = statistic_value
+
+    return statistic_value
 
 
 def boxplot_figure(data, title):
@@ -27,6 +37,11 @@ def plot_by_sex(data, column):
     boxplot_figure(male_data[column], f'{column} Hombres')
     boxplot_figure(female_data[column], f'{column} Mujeres')
 
+    return {
+        "male_data": male_data,
+        "female_data": female_data
+    }
+
 
 def plot_by_calories(data, column):
     cate1 = data[data[CALORIES_COLUMN] <= 1100]
@@ -37,14 +52,70 @@ def plot_by_calories(data, column):
     boxplot_figure(cate2[column], f'{column} CATE 2')
     boxplot_figure(cate3[column], f'{column} CATE 3')
 
+    return {
+        "cate1": cate1,
+        "cate2": cate2,
+        "cate3": cate3
+    }
+
+
+def print_statistics(title, data, column):
+    q1 = data[column].quantile(0.25)
+    q3 = data[column].quantile(0.75)
+    iqr = q3 - q1
+    print('******************************************')
+    print(title)
+    print()
+    print(f'Mean: {data[column].mean()}')
+    print(f'Mode: {data[column].mode()[0]}')
+    print(f'Median: {data[column].median()}')
+    print(f'Std. deviation: {data[column].std()}')
+    print(f'Q1: {q1} | IQR: {iqr} | Q3: {q3}')
+    print(f'Correlation:\n {data.corr()}')
+    print('******************************************')
+
 
 if __name__ == '__main__':
-    data = pd.read_excel('Datos trabajo 1.xls')
-    replace_by_mean(data, ALCOHOL_COLUMN)
-    replace_by_mean(data, FAT_COLUMN)
 
-    plot_by_sex(data, ALCOHOL_COLUMN)
-    plot_by_calories(data, ALCOHOL_COLUMN)
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename", default='Datos trabajo 1.xls',
+                        help="Path to excel file", metavar="FILE")
+    parser.add_argument("-c", "--column", dest="column", default='alcohol',
+                        help="Name of column to analyze [alcohol | fat | calories]")
+    parser.add_argument("-s", "--statistic", dest="statistic", default='mean',
+                        help="Statistic to use to replace missing values [mean | median | mode]")
+    parser.add_argument("-p", "--plot", dest="plot", default='population',
+                        help="Name of the plot [population | sex | calories]")
+
+    args = parser.parse_args()
+
+    data = pd.read_excel(args.filename)
+
+    column = ''
+    if args.column == 'alcohol':
+        column = ALCOHOL_COLUMN
+    elif args.column == 'fat':
+        column = FAT_COLUMN
+    elif args.column == 'calories':
+        column = CALORIES_COLUMN
+    else:
+        print('Invalid column, option must be either [alcohol | fat | calories]')
+        exit()
+
+    replace_by_statistic(args.statistic, data, column)
+
+    if args.plot == 'population':
+        boxplot_figure(data[column], f'Population {column}')
+        print_statistics(f'{column} Statistics', data, column)
+    elif args.plot == 'sex':
+        result = plot_by_sex(data, column)
+        print_statistics(f'Male {column} Statistics', result["male_data"], column)
+        print_statistics(f'Female {column} Statistics', result["female_data"], column)
+    elif args.plot == 'calories':
+        result = plot_by_calories(data, column)
+        print_statistics(f'CATE 1 {column} Statistics', result["cate1"], column)
+        print_statistics(f'CATE 2 {column} Statistics', result["cate2"], column)
+        print_statistics(f'CATE 3 {column} Statistics', result["cate3"], column)
 
     plt.show()
 
